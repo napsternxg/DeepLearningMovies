4995498ac6b034a3cec157a4dea85a4b808dd506
Shubhanshu Mishra <smishra8@illinois.edu>
2014-12-27 01:52:07 -0600

    Updated the code for fixing quoting and generated output using Random Forest

--- BagOfWords.py
+++ BagOfWords.py
@@ -17,13 +17,13 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    train = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'labeledTrainData.tsv'), header=0, \
-                    delimiter="\t", quoting=3)
-    test = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'testData.tsv'), header=0, delimiter="\t", \
-                   quoting=3 )
+    train = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data',
+                                     'labeledTrainData.tsv'), delimiter="\t")
+    test = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data',
+                                    'testData.tsv'), delimiter="\t")
 
     print 'The first review is:'
-    print train["review"][0]
+    print train.ix[0]
 
     raw_input("Press Enter to continue...")
 
@@ -101,7 +101,8 @@ if __name__ == '__main__':
     output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
 
     # Use pandas to write the comma-separated output file
-    output.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'Bag_of_Words_model.csv'), index=False, quoting=3)
+    output.to_csv(os.path.join(os.path.dirname(__file__), 'data',
+                               'Bag_of_Words_model.csv'), index=False,quoting=2)
     print "Wrote results to Bag_of_Words_model.csv"
 
 
